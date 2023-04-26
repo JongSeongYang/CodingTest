f186d8ae7eabbd590ae6fc93ee05598d146e5659
JongSeong Yang <yangjs0523@naver.com>
2023-04-26 17:52:16 +0900

    삼총사

---
+++ src/programmers/lv1/Pg131705.py
@@ -0,0 +1,15 @@
+# 삼총사
+
+from itertools import combinations
+def solution(number):
+    answer = 0
+    p = list(combinations(number, 3))
+
+    for l in p:
+        sum = 0
+        for i in range(3):
+            sum += l[i]
+        if sum == 0:
+            answer += 1
+
+    return answer
