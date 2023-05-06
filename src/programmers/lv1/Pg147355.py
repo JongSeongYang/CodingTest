18a6083cf1cfee6091dd7d2ad15785516994e23d
JongSeong Yang <yangjs0523@naver.com>
2023-05-06 22:33:18 +0900

    크기가 작은 부분 문자열

---
+++ src/programmers/lv1/Pg147355.py
@@ -0,0 +1,9 @@
+# 크기가 작은 부분 문자열
+
+def solution(t, p):
+    answer = 0
+    for i in range(len(t)-len(p)+1):
+        if int(t[i:i+len(p)]) <= int(p):
+            print(t[i:i+len(p)])
+            answer+=1
+    return answer
