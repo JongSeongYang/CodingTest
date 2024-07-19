87bc62a51a3e62baee0d683cabbce4510f529449
JongSeongYang <60866755+JongSeongYang@users.noreply.github.com>
2024-07-19 15:24:35 +0900

    최고의 집합

---
+++ src/programmers/java/lv3/Pg12938.java
@@ -0,0 +1,24 @@
+package programmers.java.lv3;
+
+import java.util.*;
+
+// 최고의 집합
+
+public class Pg131129 {
+    public int[] solution(int n, int s) {
+        // n이 s보다 크면 불가능한 경우
+        if(n > s) return new int[] {-1};
+        
+        int[] answer = new int[n];
+        int result = s/n;
+        int rest = s%n;
+        for(int i = n-1; i >= 0; i--){
+            answer[i] = result;
+            if(rest > 0) {
+                answer[i] += 1;
+                rest --;
+            }
+        }
+        return answer;
+    }
+}
