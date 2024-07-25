53187106cb093372325841d73fcd7146ddb71894
JongSeongYang <60866755+JongSeongYang@users.noreply.github.com>
2024-07-25 17:25:53 +0900

    하노이의 탑

---
+++ src/programmers/java/lv2/Pg12946.java
@@ -0,0 +1,23 @@
+package programmers.java.lv3;
+
+import java.util.*;
+
+// 하노이의 탑
+
+public class Pg12946 {
+    static List<int[]> arr = new ArrayList<>();
+    public int[][] solution(int n) {
+        move(n, 1, 2, 3);
+        int[][] answer = arr.stream().toArray(int[][]::new);
+        return answer;
+    }
+
+    private static void move(int cnt, int start, int mid, int end) {
+        if (cnt == 0) {
+            return;
+        }
+        move(cnt - 1, start, end, mid);
+        arr.add(new int[]{start, end});
+        move(cnt - 1, mid, start, end);
+    }
+}
